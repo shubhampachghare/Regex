860154a1d73a99f798870e8112943646f7982039
shubhampachghare <shubhampachghare@gmail.com>
2023-03-17 00:24:08 +0530

    [Shubham] -AddingEmail

--- RegexNew123/Program.cs
+++ RegexNew123/Program.cs
@@ -9,6 +9,7 @@ namespace RegexNew123
             Patterns patterns = new Patterns();
             Console.WriteLine(patterns.ValidateFName("Shubham"));
             Console.WriteLine(patterns.ValidateFName("Pachghare"));
+            Console.WriteLine(patterns.ValidateEmail("shubham.pachghare@gmail.co.in"));
 
         }
     }
