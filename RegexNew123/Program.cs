a55f45f26ce139ec896b0c7e9b26ab8f7a6db95e
shubhampachghare <shubhampachghare@gmail.com>
2023-03-16 17:43:26 +0530

    [Shubham] -AddingFname

---
+++ RegexNew123/Program.cs
@@ -0,0 +1,16 @@
+﻿using Regex_New123;
+
+namespace RegexNew123
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.ValidateFName("Shubham"));
+
+
+        }
+    }
+
+}
---
+++ RegexNew123/pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_New123
+{
+    public class Patterns
+    {
+        public static string Regex_FName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, Regex_FName);
+        }
+    }
+}
+
+
